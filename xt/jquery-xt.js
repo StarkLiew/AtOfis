ada5ed8ffe1c3fc9990cbe2c105dbae2f28c1d59
unknown <Peter@.(none)>
2010-09-30 22:05:32 +0800

    Deleted all jquery-xt

--- xt/jquery-xt.js
+++
@@ -1,9 +0,0 @@
-/*!
- * jQuery-XT 1.0.1
- *
- * Copyright (c) 2010 Kuan Yaw Liew (http://xt.atofis.com/)
- * Dual licensed under the MIT (MIT-LICENSE.txt)
- * and GPL (GPL-LICENSE.txt) licenses.
- *
- * http://xt.atofis.com/
- */
